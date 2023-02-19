9b5e47427264e101035a8b8a82f7fe56b1eb57de
vbaez49 <victor.baez1988@gmail.com>
2023-02-19 03:42:13 -0300

    proyecto inicial del proyecto la tienda de javi

---
+++ src/test/java/com/latiendadejavi/ecommerce/LatiendadejaviEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latiendadejavi.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatiendadejaviEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
